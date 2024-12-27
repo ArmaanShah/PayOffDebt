132bef418feda1386f6d013b6f4d043ba6e00141
Armaan Shah <69207811+ArmaanShah@users.noreply.github.com>
2024-12-27 15:25:25 -0500

    Update PayoffDebt.java

--- PayoffDebt.java
+++ PayoffDebt.java
@@ -1,26 +1,10 @@
 /*
  *  * NetPay.java
  * Author: Armaan Shah 
- * Submission Date: September 5th
  *
  * Purpose: The following program calculates how long it will take to pay off credit card debt based on the principle amount, the interest rate, and the monthly payment.
  * Using these values as inputs, it calculates the months needed to pay off, the total amount paid, the total interest paid, and the total amount of overpay. 
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither 
- * received nor given inappropriate assistance. I have not copied 
- * or modified code from any source other than the course webpage 
- * or the course textbook. I recognize that any unauthorized 
- * assistance or plagiarism will be handled in accordance with 
- * the University of Georgia's Academic Honesty Policy and the 
- * policies of this course. I recognize that my work is based 
- * on an assignment created by the School of Computing 
- * at the University of Georgia. Any publishing or
- * posting of source code for this assignment is strictly 
- * prohibited unless you have written consent from the 
- * School of Computing at the University of Georgia.  
- */
+
 
 import java.util.Scanner;
 
